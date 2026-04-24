9814d71e8d8435153e3cd14689027364bbdb97f9
Riva <riva0697.becse24@chitkara.edu.in>
2026-04-24 12:52:32 +0530

    new commit

--- blog-backend/config/db.js
+++ blog-backend/config/db.js
@@ -1,19 +1,22 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT   // 🔥 THIS IS IMPORTANT
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 connection.connect((err) => {
-    if (err) {
-        console.log("DB ERROR:", err);
-    } else {
-        console.log("Connected to MySQL 🔥");
-    }
+  if (err) {
+    console.log("❌ DB ERROR:", err.message);
+  } else {
+    console.log("✅ MySQL connected");
+  }
 });
 
 module.exports = connection;
